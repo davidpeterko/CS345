8eb6c5d3c3a412dc7150a9ea1e878b4889910c9d
david ko <davidpeterko@gmail.com>
2015-08-02 21:32:26 -0500

    yeah

---
+++ HW#4/ErrorChecking.hs
@@ -0,0 +1,79 @@
+module ErrorChecking where
+
+import Prelude hiding (LT, GT, EQ, id)
+import FirstClassFunctions hiding (evaluate)
+import Operators
+
+data Checked a = Good a | Error String
+  deriving Show
+
+evaluate :: Exp -> Env -> Checked Value
+evaluate (Literal v) env = Good v
+evaluate (Variable x) env =
+  case lookup x env of
+    Nothing -> Error ("Variable " ++ x ++ " undefined")
+    Just v  -> Good v
+evaluate (Unary op a) env =
+  case evaluate a env of
+    Error msg -> Error msg
+    Good av ->   checked_unary op av
+evaluate (Binary op a b) env =
+  case evaluate a env of
+    Error msg -> Error msg
+    Good av ->
+      case evaluate b env of
+        Error msg -> Error msg
+        Good bv ->
+          checked_binary op av bv
+
+
+-- if x y z -> if x then y else z
+evaluate (If x y z) env =
+  case evaluate x env of
+    Error msg -> Error msg
+    Good xv ->let BoolV temp = xv in 
+      if temp
+      then case evaluate y env of
+        Error msg -> Error msg
+        Good yv -> Good yv
+      else case evaluate z env of
+        Error msg-> Error msg
+        Good zv -> Good zv
+
+-- non-recursive var
+-- what to do here? is this for declares?
+
+
+-- function call / definitions
+
+-- function definition
+evaluate (Fun arg body) env = 
+  Good (ClosureV arg body env)
+
+-- function call
+evaluate (Call func arg) env =   
+
+execute exp = evaluate exp []
+
+checked_unary :: UnaryOp -> Value -> Checked Value
+checked_unary Not (BoolV b) = Good (BoolV (not b))
+checked_unary Neg (IntV i)  = Good (IntV (-i))
+checked_unary op   v         = 
+    Error ("Unary " ++ show op ++ " called with invalid argument " ++ show v)
+
+checked_binary :: BinaryOp -> Value -> Value -> Checked Value
+checked_binary Add (IntV a)  (IntV b)  = Good (IntV (a + b))
+checked_binary Sub (IntV a)  (IntV b)  = Good (IntV (a - b))
+checked_binary Mul (IntV a)  (IntV b)  = Good (IntV (a * b))
+checked_binary Div _         (IntV 0)  = Error "Divide by zero"
+checked_binary Div (IntV a)  (IntV b)  = Good (IntV (a `div` b))
+checked_binary And (BoolV a) (BoolV b) = Good (BoolV (a && b))
+checked_binary Or  (BoolV a) (BoolV b) = Good (BoolV (a || b))
+checked_binary LT  (IntV a)  (IntV b)  = Good (BoolV (a < b))
+checked_binary LE  (IntV a)  (IntV b)  = Good (BoolV (a <= b))
+checked_binary GE  (IntV a)  (IntV b)  = Good (BoolV (a >= b))
+checked_binary GT  (IntV a)  (IntV b)  = Good (BoolV (a > b))
+checked_binary EQ  a         b         = Good (BoolV (a == b))
+checked_binary op  a         b         = 
+    Error ("Binary " ++ show op ++ 
+           " called with invalid arguments " ++ show a ++ ", " ++ show b)
