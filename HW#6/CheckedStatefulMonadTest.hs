392e0e960ef8f611fe33007d6e7a33a569106861
David Ko <davidko@cs.utexas.edu>
2015-08-06 12:46:41 -0500

    added a few more tests cases from last assignment

--- HW#6/CheckedStatefulMonadTest.hs
+++ HW#6/CheckedStatefulMonadTest.hs
@@ -31,6 +31,17 @@ t8 = parseExp "4 + true"
 
 t9 = parseExp "4 && false"
 
+--t10 = parseExp "try{ 3 + true} catch {7}"
+t11 = parseExp "5 + 10 + true"
+--t12 = parseExp "try{true + true} catch {15 + 10}"
+t13 = parseExp "var x = 3; x"
+--t14 = parseExp "if (true) 5 else 10"
+--t15 = parseExp "if (false) 5 else 10"
+--t16 = parseExp "try{ if (5 > 1) 10 else 5} catch {100}"
+--t17 = parseExp "try{ if (5 > 7) 10 else 5} catch {100}"
+--t18 = parseExp "try{var x = 3 + false; x} catch {100}"
+--t19 = parseExp "if (8 + false) 10 else 55"
+
 
 main = do
   test "evaluate" execute t1
@@ -42,6 +53,16 @@ main = do
   test "evaluate" execute t7
   test "evaluate" execute t8
   test "evaluate" execute t9
+  --test "evaluate" execute t10
+  test "evaluate" execute t11
+  --test "evaluate" execute t12
+  test "evaluate" execute t13
+  --test "evaluate" execute t14
+  --test "evaluate" execute t15
+  --test "evaluate" execute t16
+  --test "evaluate" execute t17
+  --test "evaluate" execute t18
+  --test "evaluate" execute t19
 
 -- I combined both tests from StatefulMonadTest.hs and ErrorChecking.hs 
 -- into one file to test my CheckedStatefulMonad.hs into this file called:
